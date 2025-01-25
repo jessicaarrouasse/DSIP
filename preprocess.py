0eaf550c4d486f7b6c478e6df25e2ec6589b56cd
Sarit Meshesha <sarit@viz.ai>
2025-01-25 20:26:39 +0200

    feature extraction

--- preprocess.py
+++ preprocess.py
@@ -4,9 +4,17 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 from typing import Dict, Tuple, List, Any
+import logging
+import pickle
+
+# Set up logging configuration
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 def parse(csv_path):
-   #Load the dataset
+   # Load the dataset
    print(f"Location of the file: {csv_path}")
    df = pd.read_csv(csv_path)
    return df
@@ -40,7 +48,6 @@ def get_unique_mappings(df: pd.DataFrame) -> Tuple[Dict, Dict, Dict]:
 
    return gender_age_to_group, group_to_gender, group_to_age
 
-
 def impute_from_group_id(row: pd.Series, group_to_gender: Dict, group_to_age: Dict) -> pd.Series:
    """
    Impute gender and age_level from user_group_id.
@@ -58,7 +65,6 @@ def impute_from_group_id(row: pd.Series, group_to_gender: Dict, group_to_age: Di
 
    return row
 
-
 def impute_group_id(row: pd.Series, gender_age_to_group: Dict) -> pd.Series:
    """
    Impute user_group_id from gender and age_level.
@@ -69,7 +75,6 @@ def impute_group_id(row: pd.Series, gender_age_to_group: Dict) -> pd.Series:
          row['user_group_id'] = gender_age_to_group[key]
 
    return row
-
 
 def impute_user_info(row: pd.Series, user_to_gender: Dict, user_to_age: Dict) -> pd.Series:
    """
@@ -192,6 +197,100 @@ def split_data(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFra
    
    return train_data, test_data, val_data
 
+def compute_train_stats(train_data: pd.DataFrame) -> dict:
+   logging.info("Starting to compute statistics for training data.")
+   stats = {}
+   # Check for NaN values in the grouping columns
+   missing_values = train_data[['campaign_id', 'product', 'product_category_1', 'webpage_id']].isnull().sum()
+   logging.info(f"Missing values in grouping columns: {missing_values}")
+   # Drop rows with NaN in grouping columns
+   train_data = train_data.dropna(subset=['campaign_id', 'product', 'product_category_1', 'webpage_id'])
+   stats['campaign_ctr'] = train_data.groupby('campaign_id')['is_click'].agg('mean').fillna(0)
+   stats['product_popularity'] = train_data.groupby('product')['is_click'].agg('mean').fillna(0)
+   stats['product_category_popularity'] = train_data.groupby('product_category_1')['is_click'].agg('mean').fillna(0)
+   stats['webpage_ctr'] = train_data.groupby('webpage_id')['is_click'].agg('mean').fillna(0)
+   logging.info("Statistics computation completed.")
+   return stats
+
+def feature_extraction(df: pd.DataFrame, selected_features: List[str], train_stats: dict = None) -> Tuple[np.ndarray, np.ndarray]:
+   '''
+   The function extracts features and labels from the input DataFrame.
+   Args:
+      df (pd.DataFrame): The DataFrame containing the data.
+      selected_features (List[str]): The list of selected features to extract.
+      train_stats (dict): A dictionary containing precomputed statistics for the training set.
+   Returns:
+      Tuple[np.ndarray, np.ndarray]: A tuple containing the features (X) and labels (y) as NumPy arrays.
+   '''
+   df = df.copy()
+   logging.info("Starting feature extraction.")
+   # 1. Time-Based Features
+   df['DateTime'] = pd.to_datetime(df['DateTime'])
+   df['hour'] = df['DateTime'].dt.hour
+   df['day_of_week'] = df['DateTime'].dt.dayofweek
+   df['day_name'] = df['DateTime'].dt.day_name()
+   df['is_weekend'] = df['day_of_week'].apply(lambda x: 1 if x >= 5 else 0) # should we map to -1/1 instead of 0/1? depend on the model?
+   # Time of day feature - Define bins and labels
+   bins = [-1, 5, 11, 17, 21, 24]
+   labels = ['Late Night', 'Morning', 'Afternoon', 'Evening', 'Early Night']
+   # Bin the hours into time periods
+   df['time_period'] = pd.cut(df['hour'], bins=bins, labels=labels, right=False)
+   # Holiday feature
+   # a holiday feature for July 4th
+   df['is_holiday'] = (df['DateTime'].dt.month == 7) & (df['DateTime'].dt.day == 4)
+   # Convert boolean to 0/1
+   df['is_holiday'] = df['is_holiday'].astype(int) #should we map to -1/1? depened on model
+   # drop day_name since it's similar to day_of_week feature (day_of_week is numeric and keeps the ordinal relationship)
+   df = df.drop('day_name', axis=1)
+   # Encode time_period categorical feature for linear models
+   # Map time periods to integers
+   time_period_map = {
+      'Late Night': 0,
+      'Morning': 1,
+      'Afternoon': 2,
+      'Evening': 3,
+      'Early Night': 4
+   }
+   df['time_period'] = df['time_period'].map(time_period_map)
+   # 2. user features - demographics features
+   df['gender_binary'] = df['gender'].map({'Male': 0, 'Female': 1, None: -1}) #should we handle null here? if we use XGBoost, LightGBM we can skip handle nulls
+   df = df.drop('gender', axis=1)
+   # 3. User Behavior Features: Aggregate user-level data to capture engagement trends - lets make sure we dont have lekage here when spliting the data to train/test
+   user_session_count = df.groupby('user_id')['session_id'].nunique().rename('session_count')
+   df = df.merge(user_session_count, on='user_id', how='left')
+   avg_user_depth = df.groupby('user_id')['user_depth'].mean().rename('avg_user_depth')
+   df = df.merge(avg_user_depth, on='user_id', how='left')
+   # 4. Campaign & Product Features
+   if train_stats is not None:
+      # Use precomputed stats for test set or validation set
+      campaign_ctr = df['campaign_id'].map(train_stats['campaign_ctr'])
+      product_popularity = df['product'].map(train_stats['product_popularity'])
+      product_category_popularity = df['product_category_1'].map(train_stats['product_category_popularity'])
+      webpage_ctr = df['webpage_id'].map(train_stats['webpage_ctr'])
+   else:
+      # Drop rows with NaN in grouping columns before computing stats
+      df = df.dropna(subset=['campaign_id', 'product', 'product_category_1', 'webpage_id'])
+      # Compute stats for training set
+      campaign_ctr = df.groupby('campaign_id')['is_click'].transform('mean').fillna(0)
+      product_popularity = df.groupby('product')['is_click'].transform('mean').fillna(0)
+      product_category_popularity = df.groupby('product_category_1')['is_click'].transform('mean').fillna(0)
+      webpage_ctr = df.groupby('webpage_id')['is_click'].transform('mean').fillna(0)
+    
+   df['campaign_ctr'] = campaign_ctr
+   df['product_popularity'] = product_popularity
+   df['product_category_popularity'] = product_category_popularity
+   df['webpage_ctr'] = webpage_ctr
+   # 5. Interaction Features
+   df['engagement_city'] = df['user_depth'] * df['city_development_index']
+   df['campaign_product_ctr'] = df['campaign_ctr'] * df['product_popularity']
+   df['product_category_1_age_level'] = df['product_category_1'] * df['age_level'] 
+   
+   logging.info("Feature extraction completed.")
+   # Extract selected features and labels
+   X = df[selected_features].to_numpy()
+   y = df['is_click'].to_numpy()
+   
+   return X, y
 
 def save_dataframe(df, filename):
    # Create the 'data' folder if it doesn't exist
@@ -200,17 +299,41 @@ def save_dataframe(df, filename):
    file_path = os.path.join(output_folder, filename)
    df.to_csv(file_path, index=False)
 
+def save_pickle(data, filename):
+    # Create the 'data' folder if it doesn't exist
+    output_folder = "data"
+    os.makedirs(output_folder, exist_ok=True)
+    file_path = os.path.join(output_folder, filename)
+    # Save the data as a pickle file
+    with open(file_path, 'wb') as f:
+        pickle.dump(data, f)
+    print(f"Saved {filename}")
+
 def main(csv_path):
    # 1. Clean Data
    df = parse(csv_path)
    df = clean_data(df)
    # 2. Split Data
    train_data, test_data, validation_data = split_data(df)
+   # Save the datasets
    save_dataframe(train_data, "train.csv")
    save_dataframe(test_data, "test.csv")
    save_dataframe(validation_data, "validation.csv")
    print("Train, test and validation datasets saved")
    # 3. Feature Extraction
+   # 3.1 Compute Statistics on the Training Set
+   train_stats = compute_train_stats(train_data)
+   # 3.2 Select features - TODO: consider to move to consts
+   selected_features = ['campaign_product_ctr', 'webpage_id', 'product_category_popularity', 'product_popularity', 'var_1', 'is_weekend', 'is_holiday', 'session_count', 'product_category_1_age_level']
+   # 3.3 Feature Extraction for Training
+   X_train, y_train = feature_extraction(train_data, selected_features)
+   # 3.4 Feature Extraction for Test/Validation (using precomputed statistics)
+   X_test, y_test = feature_extraction(test_data, selected_features, train_stats)
+   X_validation, y_validation = feature_extraction(validation_data, selected_features, train_stats)
+   # Save the features and labels as pickle files
+   save_pickle((X_train, y_train), "X_train_y_train.pkl")
+   save_pickle((X_test, y_test), "X_test_y_test.pkl")
+   save_pickle((X_validation, y_validation), "X_validation_y_validation.pkl")
    
 
 if __name__ == '__main__':
@@ -218,4 +341,3 @@ if __name__ == '__main__':
    parser.add_argument("--csv-path", type=str)
    args = parser.parse_args()
    main(args.csv_path)
-
